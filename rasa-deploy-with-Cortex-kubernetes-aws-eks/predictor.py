0d335d14c445477b467c4b4b3be5130e76dada7c
Ubuntu <unileverlinuxone@Unilever-Linux-One.msn4sp1wywkuhmdoeii4ny4fve.cx.internal.cloudapp.net>
2020-09-09 21:57:18 +0000

    source code

--- rasa-deploy-with-Cortex-kubernetes-aws-eks/predictor.py
+++ rasa-deploy-with-Cortex-kubernetes-aws-eks/predictor.py
@@ -4,8 +4,8 @@ import subprocess
 
 class PythonPredictor:
     def __init__(self, config):
-        subprocess.call("python -m spacy download es_core_news_sm".split(" "))
-        self.model = Interpreter.load('models/20200821-014743/nlu') ## this should be an extracted model
+        subprocess.call("python -m spacy download en_core_news_sm".split(" "))
+        self.model = Interpreter.load('models/20200909-161655/nlu') ## this should be an extracted model
 
     def predict(self, payload):
         prediction = json.dumps(self.model.parse(payload["text"],only_output_properties=True))
